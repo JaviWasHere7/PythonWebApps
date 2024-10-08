a20ace7a4ad681b0f69657ca293169b4a7c5a7e8
JavierAArroyo-Solis <157152701+JaviWasHere7@users.noreply.github.com>
2024-10-08 15:44:55 -0600

    Initial application

--- Student/06/Superhero/config/urls.py
+++ Student/06/Superhero/config/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
+from hero.views import HeroListView, HeroDetailView, HeroCreateView, HeroUpdateView, HeroDelateView 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('hero/', HeroListView.as_view(), name='hero_list'),
+    path('hero/<int:pk>', HeroDetailView.as_view(), name='hero_detail'),
+    path('hero/add', HeroCreateView.as_view(), name='hero_add'),
+    path('hero/<int:pk>/', HeroUpdateView.as_view(), name='hero_edit'),
+    path('hero/<int:pk>/delete', HeroDelateView.as_view(), name='hero_delete'),
 ]
