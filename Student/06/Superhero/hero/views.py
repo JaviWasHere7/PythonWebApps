00f49951237c77281b340a2c12484ec53444fca1
JavierAArroyo-Solis <157152701+JaviWasHere7@users.noreply.github.com>
2024-10-08 14:54:01 -0600

    c

--- Student/06/Superhero/hero/views.py
+++ Student/06/Superhero/hero/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
-# Create your views here.
+from .models import Superhero
+
+class HeroListView(ListView):
+    template_name = 'hero/list.html'
+    model = Superhero
+    context_object_name = 'heros'
+
+class HeroDetailView(DetailView):
+    template_name = 'hero/detail.html'
+    model = Superhero
+    context_object_name = 'hero'
+
+class HeroCreateView(CreateView):
+    template_name = 'hero/add.html'
+    model = Superhero
+    fields = '__all__'
+
+class HeroUpdateView(UpdateView):
+    template_name = 'hero/edit.html'
+    model = Superhero
+    fields = '__all__'
+
+class HeroDelateView(DeleteView):
+    model = Superhero
+    template_name = 'hero/delete.html'
