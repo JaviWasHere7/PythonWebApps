e7e6719aeac369db104e91f10f819cca844a505c
JavierAArroyo-Solis <157152701+JaviWasHere7@users.noreply.github.com>
2024-11-25 15:48:27 -0700

    commit

--- Student/09/superhero_articles/articles/urls.py
+++ Student/09/superhero_articles/articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('create/', views.create_article, name='create_article'),
-    path('edit/<int:pk>/', views.edit_article, name='edit_article'),
+    path('', views.home, name='home'),  # Home page
+    path('create/', views.create_article, name='create_article'),  # Create Article
+    path('edit/<int:pk>/', views.edit_article, name='edit_article'),  # Edit Article
 ]
