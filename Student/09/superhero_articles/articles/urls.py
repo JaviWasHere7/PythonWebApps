54233af1aa328b8a351b6c29084a376c17bfd976
JavierAArroyo-Solis <157152701+JaviWasHere7@users.noreply.github.com>
2024-12-03 12:56:46 -0700

    Deleting project

--- Student/09/superhero_articles/articles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Home page
-    path('create/', views.create_article, name='create_article'),  # Create Article
-    path('edit/<int:pk>/', views.edit_article, name='edit_article'),  # Edit Article
-]
